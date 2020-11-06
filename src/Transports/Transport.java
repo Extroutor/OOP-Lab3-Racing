f98a428b940a9d4c74fad9255187d51536a1ab3a
extroutor <ahmedova.leila@list.ru>
2020-11-06 16:04:36 +0300

    new commit

--- src/Transports/Transport.java
+++ src/Transports/Transport.java
@@ -5,9 +5,9 @@ public class Transport {
     protected String name;
     protected Double speed;
 
-    public String GetName() { return ""; };
+    public String GetName() { return ""; }
 
-    public boolean CanTakePart(RaceType type) { return false; };
+    public boolean CanTakePart(RaceType type) { return false; }
 
     public double GetGroundRacingTime(double distance) { return 0; }
     public double GetAirRacingTime(double distance) { return 0; }
