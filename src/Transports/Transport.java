0ce42d9219d0907a47de3b26243803f22cec738e
extroutor <ahmedova.leila@list.ru>
2020-11-06 16:04:23 +0300

    new commit

--- src/Transports/Transport.java
+++ src/Transports/Transport.java
@@ -1,13 +1,13 @@
 package Transports;
 
-public abstract class Transport {
+public class Transport {
 
     protected String name;
     protected Double speed;
 
     public String GetName() { return ""; };
 
-    public abstract boolean CanTakePart(RaceType type);
+    public boolean CanTakePart(RaceType type) { return false; };
 
     public double GetGroundRacingTime(double distance) { return 0; }
     public double GetAirRacingTime(double distance) { return 0; }
